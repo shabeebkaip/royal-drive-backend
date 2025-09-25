bc98fce929322121c703c1bc5f4672b62e57c6cb
Shabeeb Kaip <shabeeb.k@ramzaula.com>
2025-09-25 11:31:21 +0300

    alias fix 1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { env } from '../config/env.js';
-import '../types/index.d';
+import '@/types/index.js';
 
 // Stock number generation utilities
 export * from './generateStockNumber.js';
